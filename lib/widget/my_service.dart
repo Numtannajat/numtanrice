d763161dfb43b9b52702d43e986419e19a6fbc5a
numtan <sumanee.n@hotmail.com>
2020-03-22 16:01:53 +0700

    Pass Data Sucess

--- lib/widget/my_service.dart
+++ lib/widget/my_service.dart
@@ -2,19 +2,43 @@ import 'package:flutter/material.dart';
 
 class Myservice extends StatefulWidget {
 // การโยนค่าระหว่าง state
-final String name;
-Myservice({Key key, this.name}):super(key : key);
-
+  final String name;
+  Myservice({Key key, this.name}) : super(key: key);
 
   @override
   _MyserviceState createState() => _MyserviceState();
 }
 
 class _MyserviceState extends State<Myservice> {
+// Field
+String nameLogin;
+
+
+// Method
+@override
+void initState(){
+  super.initState();
+  nameLogin = widget.name;
+}
+
+
+  Widget showNameLogin() {
+    return Column(
+      children: <Widget>[
+        Text('Login by'),
+        Text(nameLogin),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('My Service'),),
+      
+      appBar: AppBar(actions: <Widget>[showNameLogin()],
+        title: Text('My Service'),
+        backgroundColor: Colors.green.shade300,
+      ),
     );
   }
 }
