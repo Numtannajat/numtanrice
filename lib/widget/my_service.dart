2365d4c1424b97c8cac8c533e58fc25962a684f5
numtan <sumanee.n@hotmail.com>
2020-03-24 16:36:48 +0700

    Getmap

--- lib/widget/my_service.dart
+++ lib/widget/my_service.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class Myservice extends StatefulWidget {
 // การโยนค่าระหว่าง state
@@ -11,16 +14,45 @@ class Myservice extends StatefulWidget {
 
 class _MyserviceState extends State<Myservice> {
 // Field
-String nameLogin;
-
-
+  String nameLogin;
+  LocationData currentLocation;
 // Method
-@override
-void initState(){
-  super.initState();
-  nameLogin = widget.name;
+  @override
+  void initState() {
+    super.initState();
+    nameLogin = widget.name;
+    findLocation();
+  }
+
+
+Future<void> findLocation() async { 
+
+  currentLocation = await locationData();
+ 
+    print(
+      'Lat = ${currentLocation.latitude}, Lng = ${currentLocation.longitude}'
+  );
 }
 
+Future<LocationData> locationData() async {
+  var location = Location();
+  try{
+    return await location.getLocation();
+  } on PlatformException catch(e) {
+    if(e.code == 'PERMISSION_DENTED') {
+      print('Permission_denied');
+    }
+    return null;
+  }
+}
+
+  Widget myMap() {
+    return GoogleMap(
+        initialCameraPosition: CameraPosition(
+      target: const LatLng(13.667708, 100.621809),
+      zoom: 16.0,
+    ));
+  }
 
   Widget showNameLogin() {
     return Column(
@@ -34,11 +66,12 @@ void initState(){
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      appBar: AppBar(actions: <Widget>[showNameLogin()],
+      appBar: AppBar(
+        actions: <Widget>[showNameLogin()],
         title: Text('My Service'),
         backgroundColor: Colors.green.shade300,
       ),
+      body: myMap(),
     );
   }
 }
